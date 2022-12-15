c3b39460fd85f8ea168aef539072f5a8cae11e84
Seho Lee <seholee7003@gmail.com>
2022-12-16 03:24:34 +0900

    입력 받기

--- test.c
+++ test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+	char name[10];
 	printf("Hello World! \n");
 	printf("My name is Seho Lee. \n");
 	printf("What is your name? \n");
+	scanf("%s", name);
 	return 0;
 }
