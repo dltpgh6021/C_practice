43549e51df379a43dba5fd5c3bab0de56850ad39
Seho Lee <seholee7003@gmail.com>
2022-12-17 22:14:48 +0900

    boj c 1546

--- test.c
+++ test.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-	char input[1000000];
-	char *p = input;
-	char a;
-	int check[26] = {0, };
-	int top = 0, same = 0;
-
-	scanf("%s", input);
-	while (*p != '\0') {
-		a = *p;
-		if (a >= 'a')
-			a -= 'a' - 'A';
-		check[a - 'A'] += 1;
-		p++;
+	int input;
+	int score;
+	float result = 0;
+	int max = 0;
+	scanf("%d", &input);
+	for (int i = 0; i < input; i++) {
+		scanf("%d", &score);
+		if (score > max)
+			max = score;
+		result += score;
 	}
-	for (int i = 0; i < 26; i++) {
-		if ( check[top] < check[i] ) {
-			same = 0;
-			top = i;
-		}
-		else if ( check[top] == check[i] )
-			if (top != i)
-				same = 1;
-	}
-	if (same == 1)
-		printf("?");
-	else 
-		printf("%c", 'A' + top);
+	result = result / max * 100 / input;
+	printf("%f", result);
 	return 0;
 }
