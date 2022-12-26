3c0b7294b627342894f6fe823babad488a623868
Seho Lee <seholee7003@gmail.com>
2022-12-26 23:38:06 +0900

    boj c 2869

--- test.c
+++ test.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-int check(int, int);
-
 int main(void) { 
-	int a, b;
+	int a, b, v, h = 0, i = 0;;
+	scanf("%d %d %d", &a, &b, &v);
 
-	scanf("%d %d", &a, &b);
+	i = (v - b) / (a - b);
+	if((v - b) % (a - b) != 0)
+		i++;
 
-	if (a >= b) {
-		printf("%d \n", check(a, b));
-		printf("%d \n", a * b / check(a, b));
-	}
-	else if (b > a) {
-		printf("%d \n", check(b, a));
-		printf("%d \n", a * b / check(b, a));
-	}
+	printf("%d \n", i);
 
 	return 0;
-}
-
-int check(int a, int b) {
-	if (b != 0) {
-		return check(b, a % b);
-	}
-	else 
-		return a;
 }
