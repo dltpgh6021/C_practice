c7746e3955aec531ceada6e3ca4c099570b2f25c
Seho Lee <seholee7003@gmail.com>
2022-12-26 23:41:03 +0900

    boj c 11050

--- test.c
+++ test.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+int fac(int a) {
+	if (a == 0)
+		return 1;
+	else 
+		return a * fac(a - 1);
+}
+
 int main(void) { 
-	int a, b, v, h = 0, i = 0;;
-	scanf("%d %d %d", &a, &b, &v);
-
-	i = (v - b) / (a - b);
-	if((v - b) % (a - b) != 0)
-		i++;
-
-	printf("%d \n", i);
-
+	int n, k;
+	scanf("%d %d", &n, &k);
+	printf("%d \n", fac(n) / (fac(k) * fac(n - k)));
 	return 0;
 }
