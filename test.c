35244caf0cca6eac6dda147efa7104c72c580805
Seho Lee <seholee7003@gmail.com>
2023-01-05 01:30:02 +0900

    boj c 2775

--- test.c
+++ test.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-int main(void) { 
-	int a, b, c;
-	int i = 0;
-	int arr[100000] = {0, };
-	scanf("%d %d %d", &a, &b, &c);	
-	while(a != 0 && b != 0 && c != 0) {
-		getchar();
-		if(c * c == a * a + b * b || a * a == b * b + c * c || b * b == a * a + c * c)
-			arr[i] = 1;
-		else 
-			arr[i] = -1;
-		scanf("%d %d %d", &a, &b, &c);
-		i++;
+int main(void) {
+	int input;
+	int arr[1000][14];
+	int a[1000];
+	int k, n;
+	for(int i = 0; i < 1000; i++)
+		arr[i][0] = 1;
+	for (int i = 0; i < 14; i++)
+		arr[0][i] = 1;
+
+	for(int i = 1; i < 1000; i++) {
+		for(int j = 1; j < 14; j++) {
+			arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+		}
 	}
-	for (int j = 0; j < i; j++) {
-		if(arr[j] == 1)
-			printf("right \n");
-		else
-			printf("wrong \n");
+
+	scanf("%d", &input);
+	for(int i = 0; i < input; i++) {
+		scanf("%d", &k);
+		scanf("%d", &n);
+		a[i] = arr[k + 1][n - 1];
+	}
+	for(int i = 0; i < input; i++) {
+		printf("%d \n", a[i]);
 	}
 	return 0;
 }
