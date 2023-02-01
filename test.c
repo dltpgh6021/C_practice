6085bd6d012c11330db78bec6d690aefabf9023e
Seho Lee <seholee7003@gmail.com>
2023-02-02 00:00:23 +0900

    boj c 1929

--- test.c
+++ test.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-	int input;
-	int arr[1000][14];
-	int a[1000];
-	int k, n;
-	for(int i = 0; i < 1000; i++)
-		arr[i][0] = 1;
-	for (int i = 0; i < 14; i++)
-		arr[0][i] = 1;
-
-	for(int i = 1; i < 1000; i++) {
-		for(int j = 1; j < 14; j++) {
-			arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for(int i = n; i <= m; i++) {
+		for(int j = 2; j <= i; j++) {
+			if(i == 2) {
+				printf("%d \n", i);
+			}
+			if(i % j == 0) {
+				break;
+			}
+			if(j * j > i) {
+				printf("%d \n", i);
+				break;
+			}
 		}
-	}
-
-	scanf("%d", &input);
-	for(int i = 0; i < input; i++) {
-		scanf("%d", &k);
-		scanf("%d", &n);
-		a[i] = arr[k + 1][n - 1];
-	}
-	for(int i = 0; i < input; i++) {
-		printf("%d \n", a[i]);
 	}
 	return 0;
 }
