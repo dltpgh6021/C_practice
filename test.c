81f3b339da2cbc73e43921a24b7669d53c214fe0
Seho Lee <seholee7003@gmail.com>
2023-02-14 23:18:32 +0900

    boj c 1181

--- test.c
+++ test.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int compare(const void *p, const void *q) {
+	if (strlen(p) > strlen(q))
+		return 1;
+	else if (strlen(p) == strlen(q)) {
+		return strcmp(p, q);
+	}
+	else if (strlen(p) < strlen(q))
+		return -1;
+}
 
 int main(void) {
-	int n, m;
-	scanf("%d %d", &n, &m);
-	for(int i = n; i <= m; i++) {
-		for(int j = 2; j <= i; j++) {
-			if(i == 2) {
-				printf("%d \n", i);
-			}
-			if(i % j == 0) {
-				break;
-			}
-			if(j * j > i) {
-				printf("%d \n", i);
-				break;
-			}
-		}
+	int N;
+	char arr[20000][51];
+	
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%s", arr[i]);
+		getchar();
 	}
 
+	qsort(arr, N, 51, compare);
+
+	for (int i = 0; i < N; i++) {
+		if (strcmp(arr[i], arr[i + 1]) != 0)
+		printf("%s \n", arr[i]);
+	}
 
 	return 0;
 }
