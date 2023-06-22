b7ea430ffb37a3ecc39fee92d93c5c946fe1b46b
Seho Lee <seholee7003@gmail.com>
2023-06-22 13:28:44 +0900

    boj c 1654

--- test.c
+++ test.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-int compare(const void *p, const void *q) {
-	if (strlen(p) > strlen(q))
-		return 1;
-	else if (strlen(p) == strlen(q)) {
-		return strcmp(p, q);
-	}
-	else if (strlen(p) < strlen(q))
-		return -1;
-}
-
 int main(void) {
-	int N;
-	char arr[20000][51];
-	
-	scanf("%d", &N);
-
-	for (int i = 0; i < N; i++) {
-		scanf("%s", arr[i]);
-		getchar();
+	int k, n;
+	long long *klist;
+	long long max = 0, max_len = 0;
+	scanf("%d %d", &k, &n);
+	klist = (long long *)calloc(k, sizeof(long long));
+	for(int i = 0; i < k; i++) {
+		scanf("%lld", &klist[i]);
+		if(max < klist[i]) max = klist[i];
 	}
-
-	qsort(arr, N, 51, compare);
-
-	for (int i = 0; i < N; i++) {
-		if (strcmp(arr[i], arr[i + 1]) != 0)
-		printf("%s \n", arr[i]);
+	long long min = 1;
+	long long result = 0;
+	while(min <= max) {
+		result = 0;
+		long long mid = (min + max) / 2	;
+		for(int i = 0; i < k; i++) { result += klist[i] /mid; }
+		if(result >= n && mid > max_len) max_len = mid;
+		else if (result < n)max = mid - 1;
+		else min = mid  + 1;
 	}
-
+	printf("%lld \n", max_len);
 	return 0;
 }
